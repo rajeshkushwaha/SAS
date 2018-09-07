2200d9c4a59aeb6a43a3ec47eae7df4ab33dd7d5
Rajesh Kushwaha <rajesh.kushwaha@SPI.COM>
2018-09-07 19:17:14 +0530

    deleted files

--- src/SAS/ClientApp/src/app/app-routing.module.ts
+++ src/SAS/ClientApp/src/app/app-routing.module.ts
@@ -9,7 +9,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from "./components/login/login.component";
 import { HomeComponent } from "./components/home/home.component";
 import { QuestiontabComponent } from "./components/questionadmin/questiontab/questiontab.component";
-import { OrdersComponent } from "./components/orders/orders.component";
 import { SettingsComponent } from "./components/settings/settings.component";
 import { AboutComponent } from "./components/about/about.component";
 import { NotFoundComponent } from "./components/not-found/not-found.component";
@@ -20,8 +19,7 @@ import { AuthGuard } from './services/auth-guard.service';
 const routes: Routes = [
   { path: "", component: HomeComponent, canActivate: [AuthGuard], data: { title: "Home" } },
   { path: "login", component: LoginComponent, data: { title: "Login" } },  
-  { path: "questions", component: QuestiontabComponent, canActivate: [AuthGuard], data: { title: "Questions" } },  
-  { path: "orders", component: OrdersComponent, canActivate: [AuthGuard], data: { title: "Orders" } },
+  { path: "questions", component: QuestiontabComponent, canActivate: [AuthGuard], data: { title: "Questions" } },    
   { path: "settings", component: SettingsComponent, canActivate: [AuthGuard], data: { title: "Settings" } },
   { path: "about", component: AboutComponent, data: { title: "About Us" } },
   { path: "home", redirectTo: "/", pathMatch: "full" },
--- src/SAS/ClientApp/src/app/app.module.ts
+++ src/SAS/ClientApp/src/app/app.module.ts
@@ -46,7 +46,6 @@ import { AppComponent } from "./components/app.component";
 import { LoginComponent } from "./components/login/login.component";
 import { HomeComponent } from "./components/home/home.component";
 import { CustomersComponent } from "./components/customers/customers.component";
-import { OrdersComponent } from "./components/orders/orders.component";
 import { SettingsComponent } from "./components/settings/settings.component";
 import { AboutComponent } from "./components/about/about.component";
 import { NotFoundComponent } from "./components/not-found/not-found.component";
@@ -93,8 +92,7 @@ import { UploadquestionComponent } from './components/questionadmin/uploadquesti
     AppComponent,
     LoginComponent,
     HomeComponent,
-    CustomersComponent,    
-    OrdersComponent,
+    CustomersComponent,        
     SettingsComponent,
     UsersManagementComponent, UserInfoComponent, UserPreferencesComponent,
     RolesManagementComponent, RoleEditorComponent,
